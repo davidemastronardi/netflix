04b92f4b70eff3285b3e5fb219c3abbf94df5445
davide <davidemastronardi92@gmail.com>
2023-02-20 03:47:07 +0100

    wip

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -2,22 +2,29 @@ import React, { useState } from "react";
 import HamgurgerMenu from "../img/hamburgermenu.svg";
 import Logo from "../img/logonetflix.png";
 import Avatar from "../img/accountRandom/account2.png";
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
 
 const HamburgerMenu = () => {
+  const [openMenu, setOpenMenu] = useState(false);
 
-
-    const [openMenu, setOpenMenu] = useState(false);
-
-    
-  
-  
-    return (
+  return (
     <div className="w-full h-screen">
       <div className="w-full h-[70px] bg-black opacity fixed top-0"></div>
       <div className="w-full h-[70px] top-0 flex justify-between items-center p-[20px] fixed z-10">
-        <img onClick={() => setOpenMenu(!openMenu)} className="w-[25px]" src={HamgurgerMenu} alt="hamburgermenu" />
+        <img
+          onClick={() => setOpenMenu(!openMenu)}
+          className="w-[25px]"
+          src={HamgurgerMenu}
+          alt="hamburgermenu"
+        />
         <img className="w-[85px]" src={Logo} alt="logonetflix" />
         <input
           className="bg-background opacity w-[115px] h-[28px]"
@@ -26,25 +33,46 @@ const HamburgerMenu = () => {
           id=""
         />
       </div>
-      {openMenu &&<div className="w-[250px] h-screen bg-black fixed flex flex-col z-10  mt-[70px] p-[20px]  ">
-        <div className="flex gap-[10px] items-end">
-          <div className="">
-            <img className="w-[40px]" src={Avatar} alt="avatar" />
+      {openMenu && (
+        <div className="w-[250px] h-screen bg-black fixed flex flex-col z-10  mt-[70px]">
+          <Link to="/">
+            <div className="flex gap-[10px] items-end pl-[20px] pt-[20px]">
+              <div>
+                <img className="w-[40px]" src={Avatar} alt="avatar" />
+              </div>
+              <div>
+                <p className="text-[18px] font-medium">Davide</p>
+                <p className="text-[10px]">Cambia profilo</p>
+              </div>
+            </div>
+          </Link>
+          <div className="mt-[10px] pl-[20px]">
+            <ul className="font-medium text-[20px]">
+              <li>Account</li>
+              <li>Centro assistenza</li>
+              <li>Esci da Netflix</li>
+            </ul>
           </div>
-          <div className=" ">
-            <p className="text-[18px] font-medium">Davide</p>
-            <p className="text-[10px]">Cambia profilo</p>
+          <div className="border-t-[1px] mt-[20px]">
+            <ul className="font-medium mt-[20px] text-[20px] leading-[38px]  ">
+              <li className="hover:border-l-[4px] pl-[20px]">Home</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Film</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Serie Tv</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Thriller</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Anime</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Azione</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Commedie</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Fantasy</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Fantascienza</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Horror</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Documentari</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Romantici</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Musical</li>
+              <li className="hover:border-l-[4px] pl-[20px]">Drammi</li>
+            </ul>
           </div>
         </div>
-        <div className="mt-[10px]">
-          <ul className="font-medium">
-            <li>Account</li>
-            <li>Centro assistenza</li>
-            <li>Esci da Netflix</li>
-          </ul>
-          <hr className="mt-[20px]" />
-        </div>
-      </div>}
+      )}
     </div>
   );
 };
